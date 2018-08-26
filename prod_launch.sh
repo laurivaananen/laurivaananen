9337b5129f40ae14b7e6eb368bbf7010594bd216
Lauri Väänänen <lauri.e.helsinki.fi>
2018-08-26 13:58:16 +0300

    Fixed spelling

--- prod_launch.sh
+++ prod_launch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker-compose down
-docker-compose -f prod-docker-compose.yml build -d
+docker-compose -f prod-docker-compose.yml build
 docker-compose -f prod-docker-compose.yml up -d
 docker-compose exec web python manage.py collectstatic --no-input
 docker-compose exec web python manage.py makemigrations
