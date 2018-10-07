4c639c2333cfef0271a4b31884d5d8a4c14e55bb
Lauri Väänänen <lauri.e.helsinki.fi>
2018-10-07 18:32:05 +0300

    Added server name to nginx config

--- laurivaananen/settings/prod.py
+++ laurivaananen/settings/prod.py
@@ -2,8 +2,11 @@ from laurivaananen.settings.base import *
 
 DEBUG = False
 
-STATIC_ROOT = "/static_files/"
-STATIC_URL = "/static/"
+STATIC_URL = '/static/'
+STATIC_ROOT = '/static_files/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/media_files/'
 
 SECRET_KEY = 'jdfs78f4uqisd78fhu2nvjkdsn84'
 
